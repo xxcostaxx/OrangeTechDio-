4cb300ed4b6a8f25c57d5879486ae84ed0d64233
Reginaldo <reginaldoinf@gmail.com>
2022-12-13 11:38:34 -0300

    #

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -3,9 +3,9 @@ const offset = 0
 const limit = 10
 const url   = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-function convertPokemonToHtml(pokemon){
-    return 
-    `<li class="pokemon">
+function convertPokemonToLi(pokemon){  
+    return `
+    <li class="pokemon">
     <span class = "number">#001</span>
     <span clss = "name">${pokemon.name}</span>
     <div class ="detail">
@@ -21,14 +21,17 @@ function convertPokemonToHtml(pokemon){
 `
 }
 
+const pokemonList = document.getElementById('pokemonList')
 fetch(url)
-.then((response) => response.json())
-.then((jsonBody) => jsonBody.results)
-.then((pokemonList)=>{
-    debugger
-    console.log(pokemonList)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+      
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+
+            
+      }
+    
 })
-.catch((error) => console.error(error))
-
-
-
